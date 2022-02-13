2a31ba41ebf3d2ac4f45c461ba7063453779a61c
Ethan <89361954+ItsEthanH@users.noreply.github.com>
2022-02-13 14:29:26 +0000

    beginning price section

--- client/js/landingApp.js
+++ client/js/landingApp.js
@@ -2,9 +2,25 @@ const featureTabButtons = document.getElementsByClassName(
   'feature-tab__button'
 );
 
+const recipeShowcaseButtons = document.getElementsByClassName(
+  'recipe-showcase__button'
+);
+
 for (const btn of featureTabButtons) {
   btn.addEventListener('click', function () {
     btn.nextElementSibling.classList.toggle('feature-tab__content--active');
     btn.classList.toggle('feature-tab__button--active');
+  });
+}
+
+for (const btn of recipeShowcaseButtons) {
+  btn.addEventListener('click', () => {
+    if (!btn.classList.contains('recipe-showcase--active')) {
+      for (const btn of recipeShowcaseButtons) {
+        btn.classList.remove('recipe-showcase--active');
+      }
+
+      btn.classList.add('recipe-showcase--active');
+    }
   });
 }
