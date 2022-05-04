1144e29f89985fda2c9ae51df63568451c20ec2b
Ethan <89361954+ItsEthanH@users.noreply.github.com>
2022-05-04 16:29:54 +0100

    Revert "refined index recipes animation"
    This reverts commit df8cb2d398658e80f8b3e9e4f5fc975c69e014c1.

---
+++ server/static/js/index/indexRecipeCatalogue.js
@@ -0,0 +1,111 @@
+const recipes = {
+  breakfast: [
+    {
+      title: 'Berry Banana Breakfast Smoothie',
+      img: 'static/images/landing/breakfast-1.jpg',
+      description:
+        'If you have around 5 minutes to spend in the kitchen, Berry Banana Breakfast Smoothie might be a tremendous lacto ovo vegetarian recipe to try.',
+      cost: 2.04,
+      time: '5 mins',
+    },
+    {
+      title: 'Homemade Muesli Breakfast Cereal',
+      img: 'static/images/landing/breakfast-2.jpg',
+      description:
+        'Homemade Muesli Breakfast Cereal might be just the morn meal you are searching for.',
+      cost: 0.88,
+      time: '1 hour 30 mins',
+    },
+    {
+      title: 'Mini Quinoa Egg Breakfast Casserole',
+      img: 'static/images/landing/breakfast-3.jpg',
+      description:
+        "Mini Quinoan Egg Breakfast Casserole might be a good recipe to expand your hor d'oeuvre collection.",
+      cost: 1.46,
+      time: '30 mins',
+    },
+  ],
+
+  lunch: [
+    {
+      title: 'Turkey Ranch BLT',
+      img: 'static/images/landing/lunch-1.jpg',
+      description:
+        'Turkey, bacon, lettice and tomato in pita breads, dressed in ranch sauce.',
+      cost: 1.54,
+      time: '10 mins',
+    },
+    {
+      title: 'Thai Pasta Salad',
+      img: 'static/images/landing/lunch-2.jpg',
+      description:
+        'Satisfy your Asian craving, with a taste of Thailand. Works well on its own or as part of a larger meal',
+      cost: 2.02,
+      time: '45 mins',
+    },
+    {
+      title: 'Orzo Salad With Vegetables and Herbs',
+      img: 'static/images/landing/lunch-3.jpg',
+      description:
+        'Orzo rice with a selection of fresh vegetables and seasonings. Perfect for a lighter lunch.',
+      cost: 1.46,
+      time: '45 mins',
+    },
+  ],
+
+  dinner: [
+    {
+      title: 'Turkey Ranch BLT',
+      img: 'static/images/landing/lunch-1.jpg',
+      description:
+        'Turkey, bacon, lettice and tomato in pita breads, dressed in ranch sauce.',
+      cost: 1.54,
+      time: '10 mins',
+    },
+    {
+      title: 'Thai Pasta Salad',
+      img: 'static/images/landing/lunch-2.jpg',
+      description:
+        'Satisfy your Asian craving, with a taste of Thailand. Works well on its own or as part of a larger meal',
+      cost: 2.02,
+      time: '45 mins',
+    },
+    {
+      title: 'Orzo Salad With Vegetables and Herbs',
+      img: 'static/images/landing/lunch-3.jpg',
+      description:
+        'Orzo rice with a selection of fresh vegetables and seasonings. Perfect for a lighter lunch.',
+      cost: 1.46,
+      time: '45 mins',
+    },
+  ],
+
+  deserts: [
+    {
+      title: 'Turkey Ranch BLT',
+      img: 'static/images/landing/lunch-1.jpg',
+      description:
+        'Turkey, bacon, lettice and tomato in pita breads, dressed in ranch sauce.',
+      cost: 1.54,
+      time: '10 mins',
+    },
+    {
+      title: 'Thai Pasta Salad',
+      img: 'static/images/landing/lunch-2.jpg',
+      description:
+        'Satisfy your Asian craving, with a taste of Thailand. Works well on its own or as part of a larger meal',
+      cost: 2.02,
+      time: '45 mins',
+    },
+    {
+      title: 'Orzo Salad With Vegetables and Herbs',
+      img: 'static/images/landing/lunch-3.jpg',
+      description:
+        'Orzo rice with a selection of fresh vegetables and seasonings. Perfect for a lighter lunch.',
+      cost: 1.46,
+      time: '45 mins',
+    },
+  ],
+};
+
+export default recipes;
